5ac6a0b9e374b585ac848d97253929a834aed0ad
ecourse <smkang@codenuri.co.kr>
2019-03-03 16:17:40 +0900

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,26 @@
+cls
+
+@echo [101;93mg++ %* -std=c++17[0m
+@_g++ %* -std=c++17 -fno-diagnostics-color -o g++-out.exe
+
+@IF %ERRORLEVEL% EQU 0 (
+	echo [7m[ g++ result ][0m
+  	g++-out.exe
+) ELSE (
+	echo [7mError !![0m
+)
+
+@echo.
+@echo.
+
+@echo [101;93mcl %* /permissive- /std:c++17[0m
+@cl  %* /permissive- /std:c++17 /nologo /EHsc /link /out:cl-out.exe
+
+@IF %ERRORLEVEL%  EQU 0 (
+	echo [7m[ cl result ][0m
+	cl-out.exe
+)ELSE (
+	echo [7mError !![0m
+)
+
+@echo.
